ed0133f9e56ab3efb166953a119c8d4e6dfdf1a1
Syldys <syldysnya@gmail.com>
2021-03-26 16:35:28 -0400

    Add changes to 03_queries.rb

--- movie_buff/movie_buff/03_queries.rb
+++ movie_buff/movie_buff/03_queries.rb
@@ -1,7 +1,9 @@
 def what_was_that_one_with(those_actors)
   # Find the movies starring all `those_actors` (an array of actor names).
   # Show each movie's title and id.
-
+  Movie.joins(:actors)
+       .where("actors.name IN (?)", those_actors)
+       .select(:title, :id)
 end
 
 def golden_age
